4873c9421175a03fb2fb27de2ff32b3e5e1c2cde
Akenaide <kevinms1@gmail.com>
2014-05-24 09:25:12 +0200

    Apply template and add parser

--- dl_nyaa.py
+++ dl_nyaa.py
@@ -1,7 +1,17 @@
+#!/usr/bin/env python
 __author__ = 'KMS'
+from __future__ import absolute_import
 
+# -----------------------------------------------------------------------------
+#   Imports
+# -----------------------------------------------------------------------------
+import argparse
+import sys
 from html.parser import HTMLParser
 
+# -----------------------------------------------------------------------------
+#   Globals
+# -----------------------------------------------------------------------------
 
 class MyHTMLParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
@@ -14,7 +24,22 @@ class MyHTMLParser(HTMLParser):
                         print(attr_value)
 
 
+def mk_parser():
+    """ commandline parser """
+    parser = argparse.ArgumentParser(description= "Take a nyaa.se page and download torrents in this page")
+    #parser.add_argument('--verbose', '-v', action='store_true', default=False)
+    parser.add_argument('--no-verify', '-n', action='store_true', default=False)
+    #parser.add_argument('offset', type=int, nargs='?', default=0)
+    return parser
+# -----------------------------------------------------------------------------
+#   Main
+#------------------------------------------------------------------------------
+
+
 def main():
+    args = sys.argv[1:]
+    parser = mk_parser()
+    options = parser.parse_args(args)
     fname = "file.htm"
     with open(fname, 'r', encoding='utf-8') as f:
         fname_text = "".join(f.readlines())
@@ -23,3 +48,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# -----------------------------------------------------------------------------
+#    End of file
+#------------------------------------------------------------------------------
