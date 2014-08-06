ac68df0cfeea5b89475b85f7267b91cb86a0f18f
Akenaide <kevinms1@gmail.com>
2014-08-06 23:33:26 +0200

    Add a option to download and open directly after extraction

--- dl_nyaa.py
+++ dl_nyaa.py
@@ -6,7 +6,9 @@ __author__ = 'KMS'
 #   Imports
 # -----------------------------------------------------------------------------
 import click
-from  urllib.request import urlopen
+import os
+from urllib.request import urlopen, urlretrieve
+import uuid
 from html.parser import HTMLParser
 
 # -----------------------------------------------------------------------------
@@ -15,6 +17,10 @@ from html.parser import HTMLParser
 
 
 class MyHTMLParser(HTMLParser):
+    def __init__(self):
+      super().__init__(self)
+      self.file_link_dict = dict()
+
     def handle_starttag(self, tag, attrs):
         # print("tag %r \n attrs %r" % (tag, attrs))
         title = ('title', 'Download')
@@ -22,16 +28,29 @@ class MyHTMLParser(HTMLParser):
             if title in attrs:
                 for attr_name, attr_value in attrs:
                     if attr_name == 'href':
+                        self.file_link_dict["%s.torrent" % str(uuid.uuid4())] = attr_value
                         print(attr_value)
+
+    def get_link_dict(self):
+      return self.file_link_dict
 
 # -----------------------------------------------------------------------------
 #   Main
 #------------------------------------------------------------------------------
 
+def download_file(file_link_dict):
+  """ Take a list and download items then run each downloaded file
+  """
+  for key, link in file_link_dict.items():
+    fname = os.path.join(os.path.expandvars("%TEMP%"), key)
+    urlretrieve(link, fname)
+    os.startfile(fname)
+
 @click.command()
 @click.option('-f', '--file-name', 'lc_file_name', default='nyaa.html', help="give Html file")
 @click.option('-u', '--url', 'lc_url', default=None, help="give url")
-def main(lc_file_name, lc_url):
+@click.option('-d', '--download', 'download', default=False, is_flag=True, help="want to download directly")
+def main(lc_file_name, lc_url, download):
     if lc_url is None:
         with open(lc_file_name, 'r') as f:
             html_text = "".join(f.readlines())
@@ -40,7 +59,11 @@ def main(lc_file_name, lc_url):
         html_text = str(request.readlines())
 
     html_parser = MyHTMLParser()
-    html_parser.feed(html_text)
+
+    file_link_dict = html_parser.feed(html_text)
+
+    if download:
+      download_file(html_parser.get_link_dict())
 
 if __name__ == "__main__":
     main()
